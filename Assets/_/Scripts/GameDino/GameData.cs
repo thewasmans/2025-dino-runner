848b9e52a9e1f5aa8e503672820d57cd6409cf9a
thewasmans <96111319+Weshmangle@users.noreply.github.com>
2025-10-17 11:09:26 +0200

    Add data

--- Assets/_/Scripts/GameDino/GameData.cs
+++ Assets/_/Scripts/GameDino/GameData.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class GameData : BGCGameData
 {
     [field: SerializeField, Header("Main Data")] public float DefaultSpeedMove { get; private set; } = 2f;
+    [field: SerializeField] public int DefaultSeedRandom { get; private set; } = 42;
+
     [field: SerializeField, Header("Character Controller Data")] public float SpeedJump { get; private set; } = 10.0f;
     [field: SerializeField] public float SpeedFall { get; private set; } = 8.0f;
     [field: SerializeField] public float SpeedCrouch { get; private set; } = 10.0f;
     [field: SerializeField] public float MaxVertical { get; private set; } = 2.0f;
     [field: SerializeField] public float MinVertical { get; private set; } = 0.0f;
+
     [field: SerializeField, Header("Character Controller Data")] public float VerticalSizeCharacter { get; private set; } = 2.0f;
     [field: SerializeField] public float WaitingTimeToSpawnCactus { get; private set; } = 3f;
+
+    [field: SerializeField, Header("Bush & Cactus Data")] public int MinQuantityCactus { get; private set; } = 1;
+    [field: SerializeField] public int MaxQuantityCactus { get; private set; } = 3;
+    [field: SerializeField] public float SizeCactus { get; private set; } = 1.55f;
 }
