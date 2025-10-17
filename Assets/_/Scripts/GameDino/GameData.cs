6588b28a074bde3c9d5db87614911f3bd4e5fbbc
thewasmans <96111319+Weshmangle@users.noreply.github.com>
2025-10-17 09:22:37 +0200

    Fix header in GameData

--- Assets/_/Scripts/GameDino/GameData.cs
+++ Assets/_/Scripts/GameDino/GameData.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class GameData : BGCGameData
 {
-    [Header("Main Data")]
-    [field: SerializeField] public float DefaultSpeedMove { get; private set; } = 2f;
-    [Header("Character Controller Data")]
-    [field: SerializeField] public float SpeedJump { get; private set; } = 10.0f;
+    [field: SerializeField, Header("Main Data")] public float DefaultSpeedMove { get; private set; } = 2f;
+    [field: SerializeField, Header("Character Controller Data")] public float SpeedJump { get; private set; } = 10.0f;
     [field: SerializeField] public float SpeedFall { get; private set; } = 8.0f;
     [field: SerializeField] public float SpeedCrouch { get; private set; } = 10.0f;
     [field: SerializeField] public float MaxVertical { get; private set; } = 2.0f;
     [field: SerializeField] public float MinVertical { get; private set; } = 0.0f;
-    [field: SerializeField] public float VerticalSizeCharacter { get; private set; } = 2.0f;
-    [Header("Character Controller Data")]
+    [field: SerializeField, Header("Character Controller Data")] public float VerticalSizeCharacter { get; private set; } = 2.0f;
     [field: SerializeField] public float WaitingTimeToSpawnCactus { get; private set; } = 3f;
 }
